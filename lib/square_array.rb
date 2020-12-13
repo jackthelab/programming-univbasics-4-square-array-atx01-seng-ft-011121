8af3800ceae0739f8dee5284acc297a86804158d
Jacob Hampton <jhampton792@gmail.com>
2020-12-13 19:18:27 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+  # your code 
+  squared_arr = Array.new 
+  array.length.times do |index|
+    squared_arr.push(array[index] ** 2)
+  end
+  squared_arr
 end
